e4c09ae61603bdfac43358140aa12b112d59f627
luis <luishmanas@gmail.com>
2022-02-09 20:17:35 +0100

    Adding comments

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-ChapChallenge
+Coding challenge / by Luis Herranz
+
+The following documents should be reviewed:
+
+CameraTop.ipynb     	<- Board segmentation assignment
+CameraBottom.ipynb	<- Scratch/cracks assignment
+Source1_CommentedByLuis <- Commented C++ code
+
+
+To be able to run the Jupyter books, the following libs shoulb be installed in the environment:
+*numpy
+*cv2 - opencv2
+*matplotlib
+*sklearn
+
+A final note:
+
+I did not have time to build a binary file for unit testing. 
+You can however add any images to the indicated folder in the CameraTop.ipynb to test other images.
+The scratch assingment is slightly more complex 
+
+
+If any issues or complications arise while testing, please, do contact me at either:
+luishmanas@gmail.com
++34 634813585
--- Source1_CommentedByLuis.cpp
+++ Source1_CommentedByLuis.cpp
@@ -3,7 +3,7 @@
 
 /**	NOTE ***
  * 		
- * 
+ * 		Me han comentado que puedo hacer todos mis comentarios en ingles. And so I did
  * 
  * */
 
@@ -21,8 +21,6 @@
 /**
  * 	In a few words, we will be initializing an 'countOfImageBuffers' number of buffers.
  * 
-
-
  * 	unsigned short is the right value to store pixel representation, matching the 16bit depth I worked with in my challenges.
  * 
  * */
@@ -37,7 +35,7 @@ void InitializeImagesBuffers(int countOfImageBuffers)
 	_ppBufferImages = new unsigned short*[countOfImageBuffers] ;  			// 	pointer to image buffer
 	_pppLinesBufferImages = new unsigned short **[countOfImageBuffers];		//	pointer to an array of values tracking bit memory of images
 
-	// we will now initialize 
+	// we will now initialize for each buffer
 	for( int idx = 0;  idx < countOfImageBuffers; idx++ )
 	{
 		//_deviceWidth = 2048
@@ -49,11 +47,15 @@ void InitializeImagesBuffers(int countOfImageBuffers)
 		_pppLinesBufferImages[idx] = new unsigned short*[_deviceHeiofght];  // we prepare the space to store image rows
 		for(int jdx = 0; jdx < _deviceHeight; jdx++) 
 		{
-			_pppLinesBufferImages[idx][jdx] = 					// in each row/entry we 
-				_ppBufferImages[idx] + _deviceWidth*jdx  ;		// each entry should contain increments of _deviceWidth
-		}														// from 0 to _deviceWidth*_deviceWidth
+			_pppLinesBufferImages[idx][jdx] = 					// in each row/entry  contain increments of _deviceWidth
+				_ppBufferImages[idx] + _deviceWidth*jdx  ;		// _ppBufferImages[idx] is the address memory of each entry
+		}														// then we 0 to _deviceWidth*_deviceWidth to track memory reference of each value
 	}
 }
+
+// I see a risk where memory could not be allocated like by running out of memory
+// I suppose this can be verified before buffers are called
+
 
 //--------------------------------------------------------------------------------------------------------
 //
@@ -83,11 +85,11 @@ const unsigned char** const ppBuffer	// we are not allowed to modify the buffer 
 
 	//struct PalletData
 	//{
-	//	int     imageData ; //Location in ImageData vector.
+	//	int     imageData ; 		//Location in ImageData vector. Slot assigned
 	//	bool    bufferFull ;
-	//	VARIANT drawSegmentation ;
-	//	VARIANT drawBinarization ;
-	//	VARIANT drawDefectCandidate ;
+	//	VARIANT drawSegmentation ;		// 
+	//	VARIANT drawBinarization ;		//
+	//	VARIANT drawDefectCandidate ;	//	
 	//	const DeviceConfiguration* deviceConfig ;
 	//	FileFormat fileFormat ;
 	//} ;
@@ -115,31 +117,36 @@ const unsigned char** const ppBuffer	// we are not allowed to modify the buffer 
 		//then this old pallet has to be removed from memory now.
 		//_imageMemory es de tipo vector<ImageData>
 
-		// I find it strange that we just clear existing entries without having a way to check
-		// if we are done processing them. I would assume is done here
+		// I find it strange that we just clear existing entries without having a way to check (maybe validated before)
+		// if the slot to be allocated is not cleared
 		if ( !_imageMemory[slot].emptySlot )
 		{
 			prevPalletNumber = _imageMemory[slot].ownerPallet ;		// set the identifier of the previous pallet done in this slot
-			RemovePalletInformation( prevPalletNumber );			// clear
-			_pallets.erase( prevPalletNumber );
-			_classifications.erase( prevPalletNumber );
+			RemovePalletInformation( prevPalletNumber );			// clear and free pallet nested info before destroying the object
+			_pallets.erase( prevPalletNumber );						// destroy vector entry
+			_classifications.erase( prevPalletNumber );				// destroy vector entry
 
-			//Mark as empty each slot with the same pallet number
-			//and compact the iamges buffer
-			RemovePalletFromImagesBuffer( prevPalletNumber, slot ) ;
+			//Mark as empty? seems unnecessary as we are going to fill it right away
+			//**addedLine**_imageMemory[slot] = false ; <- i meant this line 
+			//Remove the current pallet with number from this slot
+			RemovePalletFromImagesBuffer( prevPalletNumber, slot ) ;	
 		}
 
-		//Mark the slot as unavaliable
+		//Mark the slot as non-empty
 		_imageMemory[slot].emptySlot = false ;
 
 		//In order to save in memory the image, it can be reused the memory
 		//previously allocated in the slot. This will be possible only if the 
 		//old image's size is the same that the new one (usually true).
+
+		// get camera resolution and pixel depth
 		int lineSize = deviceConfig->GetWidth() * deviceConfig->GetBytes();  // based on camera specs
 		int height = deviceConfig->GetHeight();
 		unsigned long newSize = height * lineSize;
 
-		// maybe the previous camera had different specs, in any case
+		// maybe the previous camera had different specs, in any case:
+
+		// In case new image size is smaller or =
 		if ( _imageMemory[slot].allocSize >= newSize )
 		{
 			//Copying the buffer in the previously allocated memory.
@@ -150,8 +157,15 @@ const unsigned char** const ppBuffer	// we are not allowed to modify the buffer 
 			}
 			_imageMemory[slot].ownerPallet = palletNumber;		// set new entry
 
+
+			// Since we will be using the same address for the buffer, we dont require:
+			//**addedLine**_imageMemory[slot].imageBuffer = newBuffer;
+			//**addedLine**_imageMemory[slot].allocSize = newSize;
+
+
 		}
-		else //The image has a bigger size from previous (or it's the 1st).
+		//	In case The image has a bigger size from previous (or it's the 1st).
+		else 
 		{
 			// and not null buffer then we clear it
 			if ( _imageMemory[slot].imageBuffer ) 
@@ -161,7 +175,7 @@ const unsigned char** const ppBuffer	// we are not allowed to modify the buffer 
 
 			//	if  
 			unsigned char* newBuffer = new unsigned char[newSize];
-			//  if couldnt allocate the memory
+			//  if couldnt allocate the memory, lets unlock the mutex and throw an exception
 			if ( !newBuffer ) 
 			{
 				ReleaseMutex( _mapmutex );
@@ -170,36 +184,36 @@ const unsigned char** const ppBuffer	// we are not allowed to modify the buffer 
 					ID_EXCEPT_RESULT_REPOSITORY_OUTOFMEMORY);
 			}
 
-
+			// line by line copy buffer
 			for (int i=0; i < height; i++)
 			{
 				memcpy( newBuffer + i*(size_t)lineSize, ppBuffer[i],
 					lineSize );
 			}
 
-			// set new
+			// set the ImageData info
 			_imageMemory[slot].imageBuffer = newBuffer;
 			_imageMemory[slot].allocSize = newSize;
 			_imageMemory[slot].ownerPallet = palletNumber;
 		}
 
-		//Connecting the memory slot to its (pallet/device) owner.
+		//Connecting the memory slot to its (pallet/device) .
 		_pallets[palletNumber][deviceName].imageData = slot;
-		_pallets[palletNumber][deviceName].bufferFull = false;
+		_pallets[palletNumber][deviceName].bufferFull = false;		// mmm when then could be the buffer full? Here it can never be full?
 		_pallets[palletNumber][deviceName].fileFormat = fileFormat;
 
 		//Storing the device configuration.
 		_pallets[palletNumber][deviceName].deviceConfig = deviceConfig;
 
-		//Storing it in permanent repository.
+		//Storing it in permanent storage or ? We already have ´_pallets´ to keep our running information
 		SavePalletInformation( palletNumber, deviceConfig, fileFormat, vMarkFirstProfile );
 
 		//-------------------------------------------------------------
-		ReleaseMutex( _mapmutex ); // mutex released
+		ReleaseMutex( _mapmutex ); // mutex released as we have been succesful
 	}
 
 	// if an error happens, report the error, set where in the execution happens
-	// and 	
+	// in this case the mutex has already been released
 	catch(Exception *e )
 	{
 		CString function = e->GetFunction() ;
